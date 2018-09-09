c4e9b52b41df1c48b563614ef678989f54839d4a
Dimsol <claudiosakaeshigemi@gmail.com>
2018-09-09 13:41:28 -0300

    48. Fetching Memes Asynchronously
    48. Fetching Memes Asynchronously

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+export const RECEIVE_MEMES = 'RECEIVE_MEMS';
+
+
+function receiveMemes(json){
+    const { memes }= json.data;
+
+    return {
+        type: RECEIVE_MEMES,
+        memes
+    }
+}
+
+function fetchMemesJson () {
+    return fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json() )
+}
+
+
+export function fettchmemes() {
+    return function(dispatch) {
+        return fetchMemesJson()
+            .then(json => dispatch(receiveMemes(json)))
+    }
+}
