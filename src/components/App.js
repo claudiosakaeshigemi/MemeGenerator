cd46f69b1e653034499f866e6555286ff449ed04
Dimsol <claudiosakaeshigemi@gmail.com>
2018-09-09 18:00:58 -0300

    51. Creating Meme Items
    51. Creating Meme Items

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,41 @@
-import React, {Component } from 'react';
-import { connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import '../styles/index.css';
+
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import MemeItem from './MemeItem';
+
+
 
 class App extends Component {
     constructor() {
         super();
         
         this.state = {
-            memeLimit:10
+            memeLimit: 10
         }
     }
+
     render () {
         return (
             <div>
                 <h2> Welcome to the Meme Generator!</h2>
                 {
-                    this.props.memes.slice(0,this.state.memeLimit).map( (meme, index) => {
-                        return (
-                            <h4 key={index} > {meme.name}</h4>
-                        )
-                    })
-                }
-                <div className="meme-button" onClick ={() => {
-                    this.setState( {memeLimit:this.state.memeLimit + 10 })
-                }}>Load 10 more memes...</div>
-            </div>
-        )
-    }
+                    this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
+                    return (
+                    <MemeItem 
+                        key={index} 
+                        meme={meme}
+                    />
+                )
+            })
+        }
+        <div className="meme-button" onClick={() => {
+            this.setState({memeLimit: this.state.memeLimit+10})
+          }}>Load 10 more memes...</div>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state){
---
+++ src/components/MemeItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class MemeItem extends Component {
+    render() {
+        return( 
+            <div className="meme-item">
+                <img
+                src={this.props.meme.url}
+                alt={this.props.meme.name}
+                className="meme-img"
+        />  
+
+                <p className="meme-txt"> 
+                    {this.props.meme.name}
+                </p>
+            </div>
+        )
+    }
+};
+
+export default MemeItem;
